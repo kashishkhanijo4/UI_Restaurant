96b79f3ca9b65d3a4a49b7642b98e2cbcbd726de
kashish <kashishkhanijo28@gmail.com>
2024-05-19 00:50:22 +0530

    export with menu.js

---
+++ src/data/data.js
@@ -0,0 +1,52 @@
+import Dosa from '../images/dosa.jpg'
+import Chola from '../images/chhola.jpg'
+import Idli from '../images/idli.jpg'
+import  panner from '../images/paneer.jpg'
+import Gujrati from '../images/gujrati.jpeg'
+import Masala from '../images/masala.jpg'
+import { Description } from '@mui/icons-material'
+import Menu from '../pages/Menu'
+
+
+export const Menulist =[
+
+    {
+        name:'Dosa',
+        Description:'A dosa is a thin, savoury crepe in South Indian cuisine made from a fermented batter of ground white gram and rice.',
+        image:Dosa,
+        price:200
+    },
+
+    {
+        name:'Masalasb Dosa',
+        Description:'A dosa is a thin, savoury crepe in South Indian cuisine made from a fermented batter of ground white gram and rice.',
+        image:Masala,
+        price:200
+    },
+    {
+        name:'Idli',
+        Description:'A dosa is a thin, savoury crepe in South Indian cuisine made from a fermented batter of ground white gram and rice.',
+        image:Idli,
+        price:300
+    },
+    {
+        name:'chola',
+        Description:'A dosa is a thin, savoury crepe in South Indian cuisine made from a fermented batter of ground white gram and rice.',
+        image:Chola,
+        price:200
+    },
+    
+    {
+        name:'Paneer',
+        Description:'A dosa is a thin, savoury crepe in South Indian cuisine made from a fermented batter of ground white gram and rice.',
+        image:panner,
+        price:250
+    },
+
+    {
+        name:'Gujrati',
+        Description:'A dosa is a thin, savoury crepe in South Indian cuisine made from a fermented batter of ground white gram and rice.',
+        image:Gujrati,
+        price:300
+    }
+]
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import Layout from '../components/layout/Layout'
+import { Box, Card, CardActionArea, CardMedia } from '@mui/material'
+import {MenuList} from '../data/data'
 
 const Menu = () => {
   return (
     <Layout>
-    <h1>menu-page</h1>
+   <Box>
+     {
+      MenuList.map(menu => (
+        <Card>
+          <CardActionArea>
+            <CardMedia sx={{minHeight:'400px'}}
+            component={'img'} src={menu.image} alt={menu.name}>
+
+            </CardMedia>
+          </CardActionArea>
+
+        </Card>
+      )) 
+     }
+   </Box>
 </Layout>
   )
 }
 
-export default Menu
+export default Menu;
